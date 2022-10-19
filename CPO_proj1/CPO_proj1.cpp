1c42d3d7244f6470812862ad06b1df1b6240b3e8
user <user@LAPTOP-K3F4L6EQ>
2022-10-19 11:17:31 +0200

    Dodaj pliki projektów.

---
+++ CPO_proj1/CPO_proj1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include<opencv2/opencv.hpp>
+#include<opencv2/highgui.hpp>
+
+
+int main()
+{
+    cv::Mat image = cv::imread("C:\\Users\\user\\OneDrive\\Pulpit\\obraz711.jpg", cv::IMREAD_GRAYSCALE);
+    int colorScale[255] = { };
+    int color;
+    int sum = 0;
+    for (int i = 0; i < image.rows; i++) {
+        for (int j = 0; j < image.cols; j++) {
+            color = (int)image.at<uchar>(i, j);
+            colorScale[color] += 1;
+        }
+    }
+    for (int i = 0; i < sizeof(colorScale); i++) {
+        sum += colorScale[i];
+    }
+    std::cout << sum;
+}
